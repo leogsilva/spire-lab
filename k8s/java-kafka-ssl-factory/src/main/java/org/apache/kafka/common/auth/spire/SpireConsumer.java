2eaefcebdd3d0caaf6732194a404f072a67ca8b0
Leonardo <leogsilva@gmail.com>
2021-01-16 11:23:54 -0300

    Fix for method signature

--- k8s/java-kafka-ssl-factory/src/main/java/org/apache/kafka/common/auth/spire/SpireConsumer.java
+++ k8s/java-kafka-ssl-factory/src/main/java/org/apache/kafka/common/auth/spire/SpireConsumer.java
@@ -1,6 +1,7 @@
 package org.apache.kafka.common.auth.spire;
 
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.io.ByteArrayInputStream;
@@ -11,7 +12,7 @@ public class SpireConsumer {
 
     public static final String X509 = "X509";
     
-    private Collection<? extends Certificate> generateCertificates(byte[] input) throws IOException {
+    private Collection<? extends Certificate> generateCertificates(byte[] input) throws IOException, CertificateException {
         return CertificateFactory.getInstance(X509).generateCertificates(new ByteArrayInputStream(input));
 
     } 
