a25b5ec672a17d756290e1500264ea295db6e553
Leonardo <leogsilva@gmail.com>
2021-01-16 10:31:28 -0300

    Adding k8s folder for kubernetes testing

---
+++ k8s/java-kafka-ssl-factory/src/main/java/org/apache/kafka/common/auth/spire/SpireConsumer.java
@@ -0,0 +1,18 @@
+package org.apache.kafka.common.auth.spire;
+
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Collection;
+
+public class SpireConsumer {
+
+    public static final String X509 = "X509";
+    
+    private Collection<? extends Certificate> generateCertificates(byte[] input) throws IOException {
+        return CertificateFactory.getInstance(X509).generateCertificates(new ByteArrayInputStream(input));
+
+    } 
+}
