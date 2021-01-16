bd1514239bbc344814b1d5ad5b98dac10793742c
Leonardo <leogsilva@gmail.com>
2021-01-16 12:34:46 -0300

    Adding agent to k8s cluster

---
+++ k8s/java-kafka-ssl-factory/src/test/java/org/apache/kafka/common/auth/spire/SpireConsumerTest.java
@@ -0,0 +1,11 @@
+package org.apache.kafka.common.auth.spire;
+
+import org.junit.Test;
+
+public class SpireConsumerTest {
+
+    @Test
+    public void testSVID() {
+        System.out.println("Helllo");
+    }
+}
