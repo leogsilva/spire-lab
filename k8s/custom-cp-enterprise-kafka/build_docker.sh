961c78bc2fe7f5e8eadf005cb1a479e5fbad2545
Leonardo <leogsilva@gmail.com>
2021-02-09 19:40:25 -0300

    Removing kafka custom implementation of ssl factory in favor of envoy proxy

--- k8s/custom-cp-enterprise-kafka/build_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IMAGE=$(echo $EXPECTED_REF | cut -d '/' -f2)
-docker build -t k3d-myregistry.localhost:5000/${IMAGE} -f custom-cp-enterprise-kafka/Dockerfile custom-cp-enterprise-kafka
-docker push k3d-myregistry.localhost:5000/${IMAGE}
