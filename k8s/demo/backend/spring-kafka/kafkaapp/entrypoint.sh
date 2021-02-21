ef6bf586f92ba5945c9735578b17882b367ff82c
Leonardo Silva <leogsilva@gmail.com>
2021-02-21 16:55:15 -0300

    fix for entrypoint on demo app

--- k8s/demo/backend/spring-kafka/kafkaapp/entrypoint.sh
+++ k8s/demo/backend/spring-kafka/kafkaapp/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mvn package -DskipTests=true &&\
-    java -Djava.security.egd=file:/dev/./urandom -jar /apikafka.jar
+    java -Djava.security.egd=file:/dev/./urandom -jar /build/target/kafka-sample-01-2.6.5.jar
